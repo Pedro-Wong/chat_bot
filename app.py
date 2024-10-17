d342ba84f23f56c5ffc2a8d50fd1e43a70949a3b
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2024-10-17 11:50:16 -0300

    New framework

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-"""
-
- Comentário 
-"""
 
 import openpyxl
 from urllib.parse import quote
 import webbrowser
 from time import sleep
 import pyautogui
+import pywhatkit as kit
+
 # Ler planilhas e guardar informações
 # webbrowser.open('https://web.whatsapp.com/')
 # sleep(5)
@@ -22,25 +20,32 @@ for linha in clientes.iter_rows(min_row=2):
     telefone4 = linha[3].value
     print(nome)
     # print('55'+str(telefone1))
-    print(telefone1)
-    print(telefone2)
-    print(telefone3)
-    print(telefone4)
+    # print(telefone1)
+    # print(telefone2)
+    # print(telefone3)
+    # print(telefone4)
     mensagem = f'Olá {nome}, boa tarde !'
-    try:
-        link_wpp = f'https://web.whatsapp.com/send?phone={'55'+str(telefone1)}&text={quote(mensagem)}'
-        webbrowser.open(link_wpp)
-        sleep(11)
-        seta = pyautogui.locateCenterOnScreen('seta.png')
-        sleep(5)
-        pyautogui.click(seta[0], seta[1]) 
-        sleep(5)
-        pyautogui.hotkey('ctrl', 'w')
-        sleep(5)
-    except:
-        print(f'não foi possível enviar a mensagem para {nome} e telefone {telefone1} ')
-        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
-            arquivo.write(f'{nome}, {telefone1}')
+    conv_tel = '+55'+str(telefone1)
+    print(conv_tel)
+    sleep(15)
+    kit.sendwhats_image(conv_tel, "anuncio.png", "Precisando de dinheiro rápido?", 7 )
+    sleep(3)
+    pyautogui.hotkey('ctrl', 'w')
+
+    # try:
+    #     link_wpp = f'https://web.whatsapp.com/send?phone={'55'+str(telefone1)}&text={quote(mensagem)}'
+    #     webbrowser.open(link_wpp)
+    #     sleep(11)
+    #     seta = pyautogui.locateCenterOnScreen('seta.png')
+    #     sleep(5)
+    #     pyautogui.click(seta[0], seta[1]) 
+    #     sleep(5)
+    #     pyautogui.hotkey('ctrl', 'w')
+    #     sleep(5)
+    # except:
+    #     print(f'não foi possível enviar a mensagem para {nome} e telefone {telefone1} ')
+    #     with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+    #         arquivo.write(f'{nome}, {telefone1}')
 
 
 
