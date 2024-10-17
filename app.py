f74e0fe531d104c882ac3155c02c1f42e4165527
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2024-10-17 10:47:45 -0300

    segundo commit

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ import openpyxl
 from urllib.parse import quote
 import webbrowser
 from time import sleep
+import pyautogui
 # Ler planilhas e guardar informações
-webbrowser.open('https://web.whatsapp.com/')
-sleep(20)
+# webbrowser.open('https://web.whatsapp.com/')
+# sleep(5)
 workbook = openpyxl.load_workbook('clientes.xlsx')
 clientes = workbook['Planilha1']
 
@@ -19,15 +20,31 @@ for linha in clientes.iter_rows(min_row=2):
     telefone2 = linha[1].value
     telefone3 = linha[2].value
     telefone4 = linha[3].value
-    # print(nome)
+    print(nome)
     # print('55'+str(telefone1))
-    # print(telefone1)
-    # print(telefone2)
-    # print(telefone3)
-    # print(telefone4)
+    print(telefone1)
+    print(telefone2)
+    print(telefone3)
+    print(telefone4)
     mensagem = f'Olá {nome}, boa tarde !'
-    link_wpp = f'https://web.whatsapp.com/send?phone={'55'+str(telefone1)}&text={quote(mensagem)}'
-    webbrowser.open(link_wpp)
+    try:
+        link_wpp = f'https://web.whatsapp.com/send?phone={'55'+str(telefone1)}&text={quote(mensagem)}'
+        webbrowser.open(link_wpp)
+        sleep(11)
+        seta = pyautogui.locateCenterOnScreen('seta.png')
+        sleep(5)
+        pyautogui.click(seta[0], seta[1]) 
+        sleep(5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(5)
+    except:
+        print(f'não foi possível enviar a mensagem para {nome} e telefone {telefone1} ')
+        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome}, {telefone1}')
+
+
+
+
 
 
 # https://web.whatsapp.com/send?phone=55126445&text="dsfsd"
